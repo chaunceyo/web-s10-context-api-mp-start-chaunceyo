a74d9b3421073892ad9f6f752d1a68a71195a1a0
Chaunceyo <chaunceyogletree725@gmail.com>
2024-06-15 14:53:54 -0400

    finished

--- frontend/components/Quotes.js
+++ frontend/components/Quotes.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { QuoteContext } from '../context/quotesContext'
 
-export default function Quotes(props) {
+export default function Quotes() {
+  
   const {
     quotes,
     highlightedQuote,
@@ -9,7 +11,8 @@ export default function Quotes(props) {
     editQuoteAuthenticity,
     setHighlightedQuote,
     toggleVisibility,
-  } = props
+  } = useContext(QuoteContext)
+
   return (
     <div id="quotes">
       <h3>Quotes</h3>
